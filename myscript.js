0b678405e7e55df055198d7a7a20c99c280ce3c7
purshottamxkumar <purshottamk0011@gmail.com>
2022-06-20 22:32:06 +0530

    added the hours field

--- myscript.js
+++ myscript.js
@@ -4,7 +4,7 @@ let refreshInterval;
 function todo() {
     clearInterval(refreshInterval);
 
-    if(document.getElementById("time").value > 1500) { 
+    if(document.getElementById("timeInHours").value > 1500) { 
         if(confirm("Minutes should be less than and equal to 1500. Enter the Minutes Again!")) {
             window.location.reload();
         }
@@ -12,28 +12,71 @@ function todo() {
     else {
         refreshInterval = setInterval(updateCountDown, 1000); // it refreshes each second!
 
-        let fullTime = document.getElementById("time").value;
-
         console.log("This code will run if the Minutes are smaller than and equal to 1500.");
 
-        const startingMinute = fullTime;
-        let timeInSec = startingMinute * 60;
-        const ele = document.getElementById("countdown"); // where to show
+        const ele = document.getElementById("countdown"); // where to show the countDown
 
+        let hourRem = document.getElementById("timeInHours").value;
+        if(hourRem >= 0 && hourRem < 10) {
+            hourRem = '0' + hourRem;
+        }
+
+        let minuteRem = document.getElementById("timeInMinutes").value;
+        if(minuteRem >= 0 && minuteRem < 10) {
+            minuteRem = '0' + minuteRem;
+        }
+
+        let secondRem = document.getElementById("timeInSeconds").value;
+        if(secondRem >= 0 && secondRem < 10) {
+            secondRem = '0' + secondRem;
+        }
 
         function updateCountDown() {
-            const minuteRem = Math.floor(timeInSec / 60);
-            let secondRem = timeInSec % 60;
+            ele.innerHTML = `${hourRem}:${minuteRem}:${secondRem}`;
 
-            secondRem = secondRem < 10 ? '0' + secondRem : secondRem;
+            // for hourRem
+            if(parseInt(secondRem) == 0 && parseInt(minuteRem) == 0) {
+                hourRem = parseInt(hourRem);
 
-            ele.innerHTML = `${minuteRem}:${secondRem}`;
+                if(hourRem == 0) {
+                    clearInterval(refreshInterval);
+                    secondfunc();
+                }
+                else {
+                    hourRem -= 1;
+                    if(hourRem >= 0 && hourRem < 10) {
+                        hourRem = '0' + hourRem;
+                    }
+                }
+            }
+            
+            // for minuteRem
+            if(parseInt(secondRem) == 0) { // we will change the minutes if and only if secondRem == 0
+                minuteRem = parseInt(minuteRem);
 
-            timeInSec--;
+                if(minuteRem == 0) {
+                    minuteRem = 59;
+                }
+                else {
+                    minuteRem -= 1;
+                    if(minuteRem  >= 0 && minuteRem < 10) {
+                        minuteRem = '0' + minuteRem;
+                    }
+                }
+            }
 
-            if(timeInSec < 0) {
-                clearInterval(refreshInterval);
-                secondfunc();
+            // for secondRem
+            if(parseInt(secondRem) == 0) {
+                secondRem = 59;
+            }
+            else if(parseInt(secondRem) > 0 && parseInt(secondRem) <= 10) {
+                secondRem = parseInt(secondRem);
+                secondRem -= 1;
+                secondRem  = '0' + secondRem;
+            }
+            else {
+                secondRem = parseInt(secondRem);
+                secondRem -= 1;
             }
         }
     }
